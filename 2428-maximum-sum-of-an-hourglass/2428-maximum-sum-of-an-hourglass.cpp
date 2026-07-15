78da3860a28494fafb7aeaaa811c9be3e749d245
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-07-15 18:33:30 +0530

    Time: 0 ms (100%), Space: 17.1 MB (26.4%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxSum(vector<vector<int>>& grid) {
+        int maxsum=0;
         int R=grid.size();
         int C=grid[0].size();
-        int maxval=0;
         for(int r=0;r<R;r++)
         {
             for(int c=0;c<C;c++)
             {
-                if(c+2<grid[r].size() && r+2<grid.size())
-                {
                 int sum=0;
-                sum+=grid[r][c]+grid[r][c+1]+grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c]+grid[r+2][c+1]+grid[r+2][c+2];
-                 maxval=max(maxval,sum);
-            }
+                if(r+2<grid.size() && c+2<grid[r].size())
+                {
+                   sum+=grid[r][c]+grid[r][c+1]+grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c]+grid[r+2][c+1]+grid[r+2][c+2];
+                }
+                maxsum=max(maxsum,sum);
             }
         }
-        return maxval;
+        return maxsum;
+        
     }
 };
