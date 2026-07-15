4a61a2fd58bd1c8798b9070e7fc3ee8aff7c2ca6
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-07-15 14:49:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (0.36%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+       int esum=0;
+       int odsum=0;
+       for(int in=1;in<=2*n;in++)
+       {
+        if(in%2==0)
+        {
+            esum+=in;
+        }
+        else
+        {
+            odsum+=in;
+        }
+       }
+       int a,b;
+      if (esum > odsum)
+    {
+    a = esum;
+    b = odsum;
+    }
+    else
+    {
+    a = odsum;
+    b = esum;
+     }
+       while(b!=0)
+       {
+        int rem=a%b;
+        a=b;
+        b=rem;
+       }
+       return a;
+
+        
+    }
+};
