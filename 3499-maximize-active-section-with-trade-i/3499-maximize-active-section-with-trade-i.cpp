262188f8c71093cde00579329db54a01db492b42
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-07-22 00:15:41 +0530

    Time: 179 ms (8.11%), Space: 175.8 MB (12.61%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.size();
+        int N=s.size();
+        string t='1' + s + '1';
+        int totalones=0;
 
-        int totalOnes = 0;
-        for (char c : s) {
-            if (c == '1')
-                totalOnes++;
-        }
-
-        // Add virtual 1
-        string t = "1" + s + "1";
-
-        // Store blocks as {character, length}
-        vector<pair<char, int>> blocks;
-
-        int cnt = 1;
-        for (int i = 1; i < t.size(); i++) {
-            if (t[i] == t[i - 1]) {
-                cnt++;
-            } else {
-                blocks.push_back({t[i - 1], cnt});
-                cnt = 1;
+        for(int in=0;in<s.size();in++)
+        {
+            if(s[in]=='1')
+            {
+                totalones++;
             }
         }
-        blocks.push_back({t.back(), cnt});
-
-        int ans = totalOnes;
-
-        // Check every 1-block surrounded by 0-blocks
-        for (int i = 1; i < blocks.size() - 1; i++) {
-            if (blocks[i].first == '1' &&
-                blocks[i - 1].first == '0' &&
-                blocks[i + 1].first == '0') {
-
-                int candidate = totalOnes + blocks[i - 1].second + blocks[i + 1].second;
-                ans = max(ans, candidate);
+        int ans=totalones;
+        vector<pair<char,int>>blocks;
+        int count=1;
+        for(int in=1;in<t.size();in++)
+        {
+            if(t[in]==t[in-1])
+            {
+                count++;
+            }
+            else
+            {
+                blocks.push_back({t[in-1],count});
+                count=1;
             }
         }
+        for(int in=1;in<blocks.size();in++)
+        {
+            if(blocks[in].first=='1' && blocks[in-1].first=='0' &&blocks[in+1].first=='0')
+            {
+                int temp=totalones+blocks[in-1].second+blocks[in+1].second;
 
+                 ans=max(ans,temp);   
+            }
+        
+        }
         return ans;
     }
 };
