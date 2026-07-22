63e5c9d80e4556447e88a6ec4c932efd260db822
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-07-22 14:54:52 +0530

    Time: 52 ms (35.19%), Space: 28.9 MB (92.66%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>res;
-        sort(nums.begin(),nums.end());
-        for(int ctr1=0;ctr1<nums.size()-2;ctr1++)
+       sort(nums.begin(),nums.end());
+       vector<vector<int>>res;
+      for(int ctr1=0;ctr1<nums.size();ctr1++)
+      {
+        if(ctr1>0 && nums[ctr1]==nums[ctr1-1])
         {
-            if(ctr1>0 && nums[ctr1]==nums[ctr1-1])
-            {
-                continue;
-            }
-            int ctr2=ctr1+1;
-            int ctr3=nums.size()-1;
-            while(ctr2<ctr3)
-            {
-                int sum=nums[ctr1]+nums[ctr2]+nums[ctr3];
-            if(sum==0)
-            {
-                res.push_back({nums[ctr1],nums[ctr2],nums[ctr3]});
-
-                while(ctr2<ctr3 && nums[ctr2]==nums[ctr2+1])
-                {
-                    ctr2++;
-                }
-                while(ctr2<ctr3 && nums[ctr3]==nums[ctr3-1])
-                {
-                    ctr3--;
-                }
-                ctr2++;
-                ctr3--;
-            }
-            else if(sum<0)
-            {
-                ctr2++;
-            }
-            else
-            {
-                ctr3--;
-            }
+            continue;
         }
+        int ctr2=ctr1+1;
+        int ctr3=nums.size()-1;
+        
+       
+        while(ctr2<ctr3)
+        {
+          int sum=0;
+          sum=nums[ctr1]+nums[ctr2]+nums[ctr3];
+          if(sum==0) 
+          {
+            res.push_back({nums[ctr1],nums[ctr2],nums[ctr3]});
+            while(ctr2<ctr3 && nums[ctr2]==nums[ctr2+1])
+            {
+             ctr2++;
+            }
+            while(ctr2<ctr3 && nums[ctr3]==nums[ctr3-1])
+            {
+             ctr3--;
+            }
+            ctr2++;
+            ctr3--;
+          }
+          else if(sum>0)
+          {
+            ctr3--;
+          }
+          else
+          {
+            ctr2++;
+          }
         }
-        return res;
+      }
+      return res;
     }
 };
