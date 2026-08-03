b05c0a4a98501baf2601c6bf014e938190f4f3f0
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-08-03 23:18:58 +0530

    Time: 165 ms (5.06%), Space: 191.4 MB (5.27%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
-    int solve(vector<int>& piles,int ind,int M,vector<int>& dp,vector<vector<int>>& memo){
-        if(memo[ind][M]!=-1){
+    int solve(vector<int>& piles,int ind,int M,vector<vector<int>>& memo,vector<int>dp,int N){
+           if(memo[ind][M]!=-1){
             return memo[ind][M];
-        }
-        if(ind+(2*M)>=piles.size()){
-            return  memo[ind][M]=dp[ind];
-        }
-        int ans=0;
-        int in=1;
-        while(in<=piles.size() && in<=2*M && ind+in<=piles.size()){
-            int newM=max(M,in);
-            int temp=dp[ind]-solve(piles,ind+in,newM,dp,memo);
-            ans=max(ans,temp);
-            in++;
-        }
-        return memo[ind][M]=ans;
+           }
+           if(ind+2*M>=N){
+            return memo[ind][M]=dp[ind];
+           }
+           int in=1;
+           int ans=0;
+            while(ind<=N && in<=2*M && ind+in<=N ){
+                int newM=max(in,M);
+                int temp=dp[ind]-solve(piles,ind+in,newM,memo,dp,N);
+                ans=max(ans,temp);
+                in++;
+            }
+            return memo[ind][M]=ans;
     }
 public:
     int stoneGameII(vector<int>& piles) {
        int M=1;
-       vector<vector<int>>memo(piles.size(),vector<int>(piles.size()+1,-1));
-       vector<int>dp(piles.size(),0);
-       dp[piles.size()-1]=piles[piles.size()-1];
-       for(int in=piles.size()-2;in>=0 ;in--){
-            dp[in]=dp[in+1]+piles[in];
-       }
-       return solve(piles,0,M,dp,memo);
+       int N=piles.size();
+       vector<vector<int>>memo(N,vector<int>(N+1,-1));
+       vector<int>dp(N);
+       dp[N-1]=piles[N-1];
+       for(int in=N-2;in>=0 ;in--) dp[in]=dp[in+1]+piles[in];
+
+       return solve(piles,0,M,memo,dp,N);
     }
 };
