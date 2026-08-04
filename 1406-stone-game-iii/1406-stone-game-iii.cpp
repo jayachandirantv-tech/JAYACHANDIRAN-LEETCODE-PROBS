9907b51a8f3ef5018cf56baacb19609cdbd55c8f
jayachandirantv-tech <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-08-04 21:34:02 +0530

    Time: 29 ms (60.73%), Space: 151.8 MB (17.15%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,29 +1,29 @@
 class Solution {
-    int solve(vector<int>& stonevalues,int ind,int N,vector<int>& memo){
-         if(ind>=N){
+    int solve(vector<int>& stonevalues,vector<int>& dp,int ind,int N){
+        if(ind>=N){
             return 0;
         }
-        if(memo[ind]!=-1){
-            return memo[ind];
+        if(dp[ind]!=-1){
+            return dp[ind];
         }
-       int in=1;
-       int temp=0;
-       int ans=INT_MIN;
-       while(ind<N && ind+in<=N && in<=3){
-        temp+=stonevalues[ind+in-1];
-        ans=max(ans,temp-solve(stonevalues,ind+in,N,memo));
-        in++;
-       }
-       return memo[ind]=ans;
+        int in=1;
+        int temp=0;
+        int ans=INT_MIN;
+        while(ind<=N && ind+in<=N && in<=3){
+            temp+=stonevalues[ind+in-1];
+            dp[ind]=ans;
+            ans=max(ans,temp-solve(stonevalues,dp,ind+in,N));
+            in++;
+        }
+        return dp[ind]=ans;
     }
 public:
     string stoneGameIII(vector<int>& stonevalues) {
-        int N=stonevalues.size();
-        vector<int>dp(N);
-        vector<int>memo(N,-1);
-        int ans=solve(stonevalues,0,N,memo);
-        if(ans>0) return "Alice";
-        else if(ans<0) return "Bob";
-        return "Tie";
+      int N=stonevalues.size();
+      vector<int>memo(N,-1);
+      int ans=solve(stonevalues,memo,0,N);
+      if(ans>0) return "Alice";
+      else if(ans<0) return "Bob";
+      return "Tie";
     }
 };
