1df72dc974482d94478bc85421374d623fca2c1a
Jayachandirantv <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-09-11 20:04:39 +0530

    Time: 6 ms (64.82%), Space: 24.6 MB (43.02%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,85 +1,77 @@
 class Solution {
 public:
-int dr[4]={-1,1,0,0};
-int dc[4]={0,0,-1,1};
 queue<pair<int,int>>q;
 int flips=0;
+int dr[4]={1,-1,0,0};
+int dc[4]={0,0,-1,1};
 void group(vector<vector<int>>& grid,vector<vector<int>>& visited){
     int R=grid.size();
     int C=grid[0].size();
     while(!q.empty()){
-        int sz=q.size();
-        while(sz--){
-            auto[r,c]=q.front();
-            q.pop();
-            for(int in=0;in<4;in++){
-                int nr=r+dr[in];
-                int nc=c+dc[in];
-                if(nr<0 || nr>=R ||nc<0 ||nc>=C){
-                    continue;
-                }
-                if(visited[nr][nc]==1){
-                    continue;
-                }
-                if(grid[nr][nc]==1){
-                    return;
-                }
-                visited[nr][nc]=1;
-                q.push({nr,nc});
+      int sz=q.size();
+      while(sz--){
+        auto[r,c]=q.front();
+        q.pop();
+         for(int in=0;in<4;in++){
+            int nr=r+dr[in];
+            int nc=c+dc[in];
+            
+            if(nr<0 ||nr>=R || nc<0 ||nc>=C || visited[nr][nc]==1){
+                continue;
             }
+            if(grid[nr][nc]==1){
+                return;
+            }
+            visited[nr][nc]=1;
+            q.push({nr,nc});
         }
-        flips++;      
+      }
+      flips++;
     }
 }
 void tofind(vector<vector<int>>& grid,vector<vector<int>>& visited){
     int R=grid.size();
-    bool canbreak=false;
-    queue<pair<int,int>>temp;
     int C=grid[0].size();
+    queue<pair<int,int>>temp;
+    bool found=false;
     for(int r=0;r<R;r++){
         for(int c=0;c<C;c++){
             if(grid[r][c]==1){
                 temp.push({r,c});
                 q.push({r,c});
                 visited[r][c]=1;
-                canbreak=true;
+                found=true;
                 break;
             }
         }
-         if(canbreak){
-                break;
-            }
+        if(found){
+            break;
+        }
     }
     while(!temp.empty()){
         auto[r,c]=temp.front();
-        visited[r][c]=1;
         temp.pop();
         for(int in=0;in<4;in++){
             int nr=r+dr[in];
             int nc=c+dc[in];
-            if(nr<0 || nr>=R ||nc<0 || nc>=C){
-                continue;
-            }
-            if(visited[nr][nc]==1){
+            
+            if(nr<0 ||nr>=R || nc<0 ||nc>=C || visited[nr][nc]==1){
                 continue;
             }
             if(grid[nr][nc]==1){
                 visited[nr][nc]=1;
-                temp.push({nr,nc});
                 q.push({nr,nc});
+                temp.push({nr,nc});
             }
         }
     }
 }
     int shortestBridge(vector<vector<int>>& grid) {
-        int R=grid.size();
-        int C=grid[0].size();
-        vector<vector<int>>visited(R,vector<int>(C,0));
-        tofind(grid,visited);
-        if(q.empty()){
-            return 0;
-        }
-        group(grid,visited);
-        return flips;
+      int R=grid.size();
+      int C=grid[0].size();
+      vector<vector<int>>visited(R,vector<int>(C,0));
+      tofind(grid,visited);
+      group(grid,visited);
+      return flips;
     }
 };
