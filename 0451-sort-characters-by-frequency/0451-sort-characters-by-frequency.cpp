fc67982699252dc14657e7af8807835ad7af2260
Jayachandirantv <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-09-23 22:32:21 +0530

    Time: 7 ms (20.25%), Space: 11.1 MB (53.32%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        int N=s.size();
+        unordered_map<char,int>mp;
+        set<char>st;
+        for(int in=0;in<N;in++){
+            mp[s[in]]++;
+            st.insert(s[in]);
+        }
+        priority_queue<pair<int,char>>q;
+        for(auto ch:st){
+            q.push({mp[ch],ch});
+        }
+        string res="";
+        while(!q.empty()){
+            auto[occur,ch]=q.top();
+            q.pop();
+            for(int in=0;in<occur;in++){
+                  res+=ch;
+            }
+        }
+    return res;
+    }
+};
