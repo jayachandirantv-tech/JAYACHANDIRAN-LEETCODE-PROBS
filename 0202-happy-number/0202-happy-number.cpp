0dd32c5ba13e7b2b4716d8411c7ad463fe5c1f3a
Jayachandirantv <240425934+jayachandirantv-tech@users.noreply.github.com>
2026-09-26 09:17:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.34%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        while(true) {
+            int sum = 0;
+            while(n > 0) {
+                int dig = n % 10;
+                sum += dig * dig;
+                n = n / 10;
+            }
+            if(sum == 1) {
+                return true;
+            }
+            if(sum == 4) {
+                return false;
+            }
+            n = sum;
+        }
+    }
+};
